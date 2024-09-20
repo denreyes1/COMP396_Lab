d57552091f34140e6c677765705caf8c0d8e0252
Densho <denj.rey.c@gmail.com>
2024-09-19 22:59:17 -0400

    Lab 3: Patrolling working

--- Assets/Scripts/NPCController2.cs
+++ Assets/Scripts/NPCController2.cs
@@ -29,7 +29,7 @@ public class NPCController2 : MonoBehaviour
     public Transform nextWaypoint; //
 
     public float TOL = 0.001f; // when do we consider NPC is IN the next WP.
-    //Func<int, int, int> nextWaypointIndex = (i, NwP) => (i + 1) % NwP; //
+    Func<int, int, int> nextWaypointIndex = (i, NwP) => (i + 1) % NwP; //
     public int NumberOfWaypoints;
 
     // rotation speed
@@ -38,13 +38,13 @@ public class NPCController2 : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        //originalY = this.transform.position.y;
-        //currentWaypointIndex = 0;
-        //currentWaypoint = Waypoints[currentWaypointIndex];
-        //nextWaypoint = Waypoints[nextWaypointIndex(currentWaypointIndex, NumberOfWaypoints)];
 
-        //NumberOfWaypoints = Waypoints.Length;
-        
+        NumberOfWaypoints = Waypoints.Length;
+        originalY = this.transform.position.y;
+        currentWaypointIndex = 0;
+        currentWaypoint = Waypoints[currentWaypointIndex];
+        nextWaypoint = Waypoints[nextWaypointIndex(currentWaypointIndex, NumberOfWaypoints)];
+
         sqrDistanceCutoff = distanceCutOff * distanceCutOff;
         stateMachine = new StateMachine();
 
@@ -136,7 +136,7 @@ public class NPCController2 : MonoBehaviour
     {
         Debug.Log("EvadeEnemy"); //MOCK
         Vector3 headingNPC2Opponent = goOpponent.transform.position - this.transform.position;
-        
+
         // float distance = headingNPC2Opponent.magnitude;
         float sqrDistance = headingNPC2Opponent.sqrMagnitude;
         headingNPC2Opponent.Normalize(); // make it a unit vector
@@ -145,21 +145,21 @@ public class NPCController2 : MonoBehaviour
 
     private void FollowPatrolPath()
     {
-        print($"In FollowPatrolPath"); //MOCK
-        //Vector3 heading2NextWP = nextWaypoint.position - currentWaypoint.position;
-        //if (Vector3.SqrMagnitude(heading2NextWP) < TOL) // NPC is close to the nextWP, switch accordingly
-        //{
-        //    currentWaypointIndex = nextWaypointIndex(currentWaypointIndex, NumberOfWaypoints);
-        //    currentWaypoint = Waypoints[currentWaypointIndex];
-        //    nextWaypoint = Waypoints[nextWaypointIndex(currentWaypointIndex, NumberOfWaypoints)];
-        //    //rotate towards the new waypoint
-        //    Quaternion targetRotation = Quaternion.LookRotation(heading2NextWP);
-        //    this.transform.rotation = Quaternion.Slerp(this.transform.rotation, targetRotation, Time.deltaTime * rotSpeed);
-        //}
-        //heading2NextWP.y = 0f;
-        //this.transform.Translate(heading2NextWP * Time.deltaTime * speed);
-
+        Vector3 heading = currentWaypoint.position - this.transform.position;
+        float distance = heading.magnitude;
+        if (distance < TOL)
+        {
+            currentWaypointIndex = nextWaypointIndex(currentWaypointIndex, NumberOfWaypoints);
+            currentWaypoint = Waypoints[currentWaypointIndex];
+            nextWaypoint = Waypoints[nextWaypointIndex(currentWaypointIndex, NumberOfWaypoints)];
+        }
+        else
+        {
+            heading.Normalize(); // make it a unit vector
+            transform.Translate(heading * speed * Time.deltaTime);
+        }
     }
+
 
     private bool StrongerThanEnemy()
     {
